2b5c10d8bddda6859a07ed037c113a58dd4e6766
mfrate28 <mfrate@uci.edu>
2014-02-11 11:06:46 -0800

    printing colored numbers

--- hilite.js
+++ hilite.js
@@ -1,1 +1,19 @@
-alert('hilite is ready to go');
+alert("ah");
+
+function takeValue(val) {
+    var value = val;
+    //   var value = document.getElementById('input');
+if (value > 20) {
+    value.fontcolor('red');
+}
+else if (value < 20 && value > 15) {
+    value.fontcolor('yellow');
+}
+else {
+    value.fontcolor('black');
+} 
+return value;
+}
+
+//var input = prompt("Enter Number");
+//takeValue(input);
